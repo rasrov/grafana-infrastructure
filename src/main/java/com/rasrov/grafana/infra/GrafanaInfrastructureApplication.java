bc361a7bc110a2dc070b43666a52515d7a36c786
rakhmed <rabdulaev@disid.com>
2023-06-26 17:29:41 +0200

    init commit

---
+++ src/main/java/com/rasrov/grafana/infra/GrafanaInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package com.rasrov.grafana.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafanaInfrastructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rasrov/grafana/infra/GrafanaInfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasrov.grafana.infra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaInfrastructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
