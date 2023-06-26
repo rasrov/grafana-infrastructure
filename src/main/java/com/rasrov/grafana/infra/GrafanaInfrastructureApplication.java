720d664101e39a660344149322959471a3617516
rakhmed <rabdulaev@disid.com>
2023-06-27 01:00:17 +0200

    Implemented prometheus infrastructure

--- src/main/java/com/rasrov/grafana/infra/GrafanaInfrastructureApplication.java
+++ src/main/java/com/rasrov/grafana/infra/GrafanaInfrastructureApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GrafanaInfrastructureApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GrafanaInfrastructureApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GrafanaInfrastructureApplication.class, args);
+  }
 
 }
